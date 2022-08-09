24d50da419c3fc3b1947092106e1959ec60eb0f5
Dudekuladasta <108525517+Dudekuladasta@users.noreply.github.com>
2022-08-09 22:42:53 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIARIVWGUU5Y2Q2J5VZ"
-  secret_key = "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"
+  access_key = "AKIARIVWGUU53DSJXC4T"
+  secret_key = "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"
 }
